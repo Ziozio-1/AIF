1f2adcda82ac76f9f53c940facf580aea8d97e36
enzio <enziom788@gmail.com>
2025-10-06 14:10:21 +0200

    TP1 avancée

--- developpement/MNIST/model.py
+++ developpement/MNIST/model.py
@@ -4,18 +4,21 @@ import torch.nn.functional as F
 
 class MNISTNet(nn.Module):
     def __init__(self):
-        super(MNNISTNet, self).__init__()
-        self.conv1 = nn.Conv2d(...)
-        self.conv2 = nn.Conv2d(...)
-        self.pool = nn.MaxPool2d(...)
-        self.fc1 = nn.Linear(...)
-        self.fc2 = nn.Linear(...)
-        self.fc3 = nn.Linear(...)
+        super(MNISTNet, self).__init__()
+        self.conv1 = nn.Conv2d(1,6,(5,5))
+        self.conv2 = nn.Conv2d(6,16,(5,5))
+        self.pool = nn.MaxPool2d((5,5))
+        self.fc1 = nn.Linear(256,128)
+        self.fc2 = nn.Linear(128,64)
+        self.fc3 = nn.Linear(64,10)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))       # First convolution followed by
-        x = self.pool(x)                # a relu activation and a max pooling#
-        x = ...
-        ...
+        x = torch.nn.MaxPool2d(x,(2,2))                # a relu activation and a max pooling#
+        x = F.relu(self.conv2(x))
+        x = torch.nn.MaxPool2d(x,(2,2))
+        x = torch.flatten(x,1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
--- developpement/MNIST/train.py
+++ developpement/MNIST/train.py
@@ -11,9 +11,6 @@ from tqdm import tqdm
 
 from model import MNISTNet
 
- # setting device on GPU if available, else CPU
-  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
 def train(net, optimizer, loader, epochs=10):
     criterion = nn.CrossEntropyLoss()
     for epoch in range(epochs):
@@ -41,3 +38,42 @@ def test(model, dataloader):
             total += y.size(0)
     return test_corrects / total
 	
+if __name__=='__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--exp_name', type=str, default = 'MNIST', help='experiment name')
+    parser.add_argument('--Batch_Size', type=int,default=1,help='Provide a specific batch Size. Default to 1.')
+    parser.add_argument('--Learning_Rate', type=float, default=0.1, help='Provide the Learning rate of the model. Default to 0.1.')
+    parser.add_argument('--Num_Epochs', type=int, default=10, help='Number of Epoch. Default to 10.')
+
+    args = parser.parse_args()
+    exp_name= args.exp_name
+    epochs = args.Num_Epochs
+    batch_size = args.Batch_Size
+    lr = args.Learning_Rate
+
+# setting device on GPU if available, else CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# transforms
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))])
+
+# datasets
+trainset = torchvision.datasets.MNIST('./data', download=True, train=True, transform=transform)
+testset = torchvision.datasets.MNIST('./data', download=True, train=False, transform=transform)
+
+# dataloaders
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+net = MNISTNet().to(device)
+optimizer = optim.SGD(net.parameters(),lr=lr)
+
+train(net, optimizer, trainloader, epochs=epochs)
+test_acc = test(net,testloader)
+print(f'Test accuracy:{test_acc}')
+
+torch.save(net.state_dict(), 'weights/mnist_net.pth')
